7bce914b266ea47bacb96fa1429a67372430a128
Monzurul Islam <mounzrul@gmail.com>
2023-01-16 15:25:15 +0600

    my portfolio

---
+++ src/components/Hero.js
@@ -0,0 +1,67 @@
+import React from "react";
+import HeroImg from "../assets/hero-img.png";
+
+import {
+  AiOutlineTwitter,
+  AiOutlineYoutube,
+  AiOutlineFacebook,
+} from "react-icons/ai";
+
+const Hero = () => {
+  return (
+    <section className="bg-primary px-5 text-white py-32">
+      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+        <div className="hero-info pb-5 md:pb-0">
+          <h1 className="text-4xl lg:text-6xl">
+            Hi, <br />I am <span className="text-accent">Monzurul</span>Islam
+            <br />
+            MERN web app Developer
+          </h1>
+
+          <p className="py-5">Let's code with Express mongoose & react-redux</p>
+
+          <div className="flex py-5 ">
+            <a
+              href="https://twitter.com/CoderMonzurul Islam"
+              className="pr-4 inline-block text-accent hover:text-white"
+            >
+              {" "}
+              <AiOutlineTwitter size={40} />{" "}
+            </a>
+            <a
+              href="https://www.youtube.com/@coderMonzurul Islam"
+              className="pr-4 inline-block text-accent hover:text-white"
+            >
+              {" "}
+              <AiOutlineYoutube size={40} />{" "}
+            </a>
+            <a
+              href="https://www.facebook.com/CoderMonzurul Islam/"
+              className="pr-4 inline-block text-accent hover:text-white"
+            >
+              {" "}
+              <AiOutlineFacebook size={40} />{" "}
+            </a>
+          </div>
+
+          <a
+            href="/#projects"
+            className=" btn bg-accent  border-2 border-[#7477FF] text-white px-6 py-3 hover:bg-transparent"
+          >
+            See Projects
+          </a>
+        </div>
+
+        <div className="hero-img">
+          <img
+            src={HeroImg}
+            alt="coding illustration"
+            className="lgw-[80%] ml-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
